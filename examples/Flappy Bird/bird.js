5cfa1c617c13bae8a2ab446f5e05eb2db1242897
Ahmet_Faruk_ASLANTAS <ahmetfarukmi@gmail.com>
2019-07-01 22:29:44 +0300

    Added more comments

--- examples/Flappy Bird/bird.js
+++ examples/Flappy Bird/bird.js
@@ -1,9 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Neuro-Evolution Flappy Bird
-
 class Bird {
   constructor() {
     this.y = height / 2;
--- examples/Flappy Bird/pipe.js
+++ examples/Flappy Bird/pipe.js
@@ -1,9 +1,3 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-
-// Neuro-Evolution Flappy Bird
-
 class Pipe {
 
   constructor() {
