241a4e9d51bbdf7e04cf8aa180be45303cbea89a
Ahmet_Faruk_ASLANTAS <ahmetfarukmi@gmail.com>
2019-07-05 14:55:31 +0300

    Bug fix: Bias weights was getting added multiple times. Version change. New test (not yet finished)

--- test/NEAT.test.js
+++ test/NEAT.test.js
@@ -1,5 +1,21 @@
-const neat = require('../lib/NEAT.js');
+const NEAT = require('../lib/NEAT.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+test('Does network math work correctly?', () => {
+  var neat = new NEAT([3, 2], 10, 0, "tanh");
+
+
+  // Since we are testing let's set all weights to 1 and all the input values to 0.1.
+
+
+
+  for (let i = 0; i < neat.popsize; i++) {
+	neat.setInputs([1, 1, 1], i);
+  }
+
+  neat.predict();
+
+  var expected_output = 0.999329299739067;
+
+
+  expect(neat.population[0][1][neat.population[0][1].length - 1][0]).toBe(expected_output);
 });
