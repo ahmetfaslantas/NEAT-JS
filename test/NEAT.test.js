84ef03bc1d9be9c7b52ea06f08536628dcc0956c
Ahmet_Faruk_ASLANTAS <ahmetfarukmi@gmail.com>
2019-07-05 17:46:00 +0300

    Added test to test network matrix math.

--- test/NEAT.test.js
+++ test/NEAT.test.js
@@ -1,21 +1,40 @@
 const NEAT = require('../lib/NEAT.js');
 
 test('Does network math work correctly?', () => {
-  var neat = new NEAT([3, 2], 10, 0, "tanh");
+	var neat = new NEAT([3, 2], 10, 0, "tanh");
 
 
-  // Since we are testing let's set all weights to 1 and all the input values to 0.1.
+	// Since we are testing let's set all weights to 1 and all the input values to 1.
+
+	for (let i = 0; i < neat.popsize; i++) {
+		for (let q = 0; q < neat.population[i][0].length; q++) {
+			for (let w = 0; w < neat.population[i][0][q].length; w++) {
+				for (let e = 0; e < neat.population[i][0][q][w].length; e++) {
+					neat.population[i][0][q][w][e] = 1;
+				}
+			}
+		}
+	}
+
+	for (let i = 0; i < neat.popsize; i++) {
+		for (let q = 0; q < neat.population[i][2].length; q++) {
+			for (let w = 0; w < neat.population[i][2][q].length; w++) {
+				neat.population[i][2][q][w] = 1;
+			}
+		}
+	}
+	
 
 
 
-  for (let i = 0; i < neat.popsize; i++) {
-	neat.setInputs([1, 1, 1], i);
-  }
+	for (let i = 0; i < neat.popsize; i++) {
+		neat.setInputs([1, 1, 1], i);
+	}
 
-  neat.predict();
+	neat.predict();
 
-  var expected_output = 0.999329299739067;
+	var expected_output = 0.999329299739067;
 
 
-  expect(neat.population[0][1][neat.population[0][1].length - 1][0]).toBe(expected_output);
+	expect(neat.population[0][1][neat.population[0][1].length - 1][0]).toBe(expected_output);
 });
